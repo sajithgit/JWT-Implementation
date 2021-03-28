77e109ce18f4c847cfed1d65d50acc0398f3c0d9
Sajith <ksajith4u@gmail.com>
2021-03-28 19:13:52 +0400

    Updated Controller to send token in Header Response and validateToken API to send the token in Header under Authorization

--- src/main/java/com/internal/jwtapi/controller/UserController.java
+++ src/main/java/com/internal/jwtapi/controller/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
 @RestController
@@ -36,42 +38,50 @@ public class UserController {
 
     @PostMapping(value = "generateToken")
     @ResponseBody
-    public String generateToken(@RequestBody UserRequest userRequest) {
+    public String generateToken(@RequestBody UserRequest userRequest, HttpServletResponse response) {
         logger.info("POST User Creation Controller" + userRequest);
         userService.createUser(userRequest);
-        String response = "Success";
-        //ResponseBody rb = ResponseBody;
-
         String token = JWT.create()
                 .withSubject(userRequest.getUserName())
                 .withClaim("password",userRequest.getPassword())
+                .withClaim("role","ADMIN")
                 .withExpiresAt(new Date(System.currentTimeMillis() + AuthenticationConstants.EXPIRATION_TIME))
                 .sign(Algorithm.HMAC512(AuthenticationConstants.SECRET.getBytes()));
-
-        return token;
+        response.addHeader(AuthenticationConstants.HEADER_STRING, AuthenticationConstants.TOKEN_PREFIX + token);
+        return "Token Generated";
     }
 
     @PostMapping(value = "validateToken")
     @ResponseBody
-    public String validateToken(@RequestBody UserRequest userRequest)
+    public String validateToken(@RequestBody UserRequest userRequest, HttpServletRequest request)
     {
-        String token = userRequest.getToken();
-        logger.info("Token Received as::"+token);
+
+        String token = request.getHeader(AuthenticationConstants.HEADER_STRING);
         String username;
         String password;
-        try {
-            DecodedJWT verify = JWT.require(Algorithm.HMAC512(AuthenticationConstants.SECRET.getBytes()))
-                    .build()
-                    .verify(token.replace(AuthenticationConstants.TOKEN_PREFIX, ""));
-
-            username = verify.getSubject();
-            password = verify.getClaim("password").asString();
-            String role = verify.getClaim("role").asString();
-        }catch (SignatureVerificationException signException)
+        //token = userRequest.getToken();
+        if(token!=null)
         {
-            logger.error("Token tampered");
-            return "invalidToken";
+            logger.info("Token Received as::"+token);
+
+            try {
+                DecodedJWT verify = JWT.require(Algorithm.HMAC512(AuthenticationConstants.SECRET.getBytes()))
+                        .build()
+                        .verify(token.replace(AuthenticationConstants.TOKEN_PREFIX, ""));
+
+                username = verify.getSubject();
+                password = verify.getClaim("password").asString();
+                String role = verify.getClaim("role").asString();
+            }catch (SignatureVerificationException signException)
+            {
+                logger.error("Token tampered");
+                return "invalidToken";
+            }
+        }else
+        {
+            return "No Token in Header";
         }
+
         return "valid : "+username + " Password :"+password;
     }
 }
